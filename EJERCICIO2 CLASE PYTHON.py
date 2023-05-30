28e9d0c7eb50cf5766ab710de24ae1fe4983cf7f
Julianvallej00214 <134751762+Julianvallej00214@users.noreply.github.com>
2023-05-30 10:38:26 -0500

    Create EJERCICIO2 CLASE PYTHON.py

---
+++ EJERCICIO2 CLASE PYTHON.py
@@ -0,0 +1,12 @@
+# DECLARACION E INICIALIZACION DE VARIABLES
+v_numuno =0
+v_numdos = 0
+v_sumnum = 0
+V_mulnum = 0
+print("OPERACIONES BASICAS CON DOS NUMEROS ")
+print(" ")
+print("INGRESE UN NUMERO: ")
+v_numuno = int(input())
+print(" ")
+print("USTED DIGITÓ: ",v_numuno)
+print(" ")
