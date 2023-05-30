6ad49a2da1625710b5a59f65d88c53da02d3de47
Julianvallej00214 <134751762+Julianvallej00214@users.noreply.github.com>
2023-05-30 10:38:50 -0500

    Create EJERCICIO3 CLASE PYTHON.py

---
+++ EJERCICIO3 CLASE PYTHON.py
@@ -0,0 +1,13 @@
+num1=int(input("DIGITE EL PIMER VALOR: "))
+num2=int(input("DIGITE EL SEGUNDO VALOR: "))
+print("_________________________________________________________________________________________ ")
+suma=num1+num2
+producto=num1*num2
+print(" ")
+print("LA SUMA DE LOS DOS VALORES ES")
+print(suma)
+print(" ")
+print("EL PRODUCTO DE LOS DOS VALORES ES")
+print(producto)
+print(" ")
+print("_________________________________________________________________________________________ ")
